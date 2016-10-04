f3cc575d888a3ca7ade1b14d5c2c2feacd7b32c7
hmallen <allenhm@gmail.com>
2016-10-04 19:01:58 -0400

    Continued work on framework.

--- hab.ino
+++ hab.ino
@@ -1,15 +1,24 @@
 /*
  * High-Altitude Balloon
  * 
+ * Features:
  * - Sensor data acquisition
  * - Relay control
  * - SD data logging
  * - SMS notifications
+ * 
+ * Sensors:
+ * - GPS
+ * - Gas Sensors
+ * - Temp/Humidity (SHT11)
+ * - Light
+ * - Accelerometer/Gyroscope/Magnetometer
+ * - Barometer (Altitude/Temperature)
  */
 
 // Libraries
 #include <SdFat.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <TinyGPS.h>
 #include <Wire.h>
@@ -21,13 +30,33 @@
 // Analog Pins
 
 // Constants
+const int chipSelect = SS;
+
+String log_file = "hab_log.txt";
+
+// Initializers
+
 
 void setup() {
+  // PIN MODES AND DIGITAL WRITES TO INITIALIZE
+  
   Serial.begin(9600);
 
-  
+  Serial.print("Initializing SD card...");
+  if (!sd.begin(chipSelect, SPI_HALF_SPEED)) {
+    sd.initErrorHalt();
+  }
+  Serial.println("success.");
 }
 
 void loop() {
   // Stuff & Things
+}
+
+void logData(String dataString) {
+  if (!logFile.open(log_file, O_RDWR | O_CREAT | O_AT_END)) {
+    sd.errorHalt("Opening log file for write failed");
+  }
+  logFile.println(dataString);
+  logFile.close();
 }
