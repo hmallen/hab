ab109b866fe7b07f9e77ebe1367aee6da2fabce1
hmallen <allenhm@gmail.com>
2016-10-04 00:15:57 -0400

    Added framework.

---
+++ hab.ino
@@ -0,0 +1,33 @@
+/*
+ * High-Altitude Balloon
+ * 
+ * - Sensor data acquisition
+ * - Relay control
+ * - SD data logging
+ * - SMS notifications
+ */
+
+// Libraries
+#include <SdFat.h>
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <TinyGPS.h>
+#include <Wire.h>
+
+// Definitions
+
+// Digital Pins
+
+// Analog Pins
+
+// Constants
+
+void setup() {
+  Serial.begin(9600);
+
+  
+}
+
+void loop() {
+  // Stuff & Things
+}
