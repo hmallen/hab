02dac25c2fb45de2aa9ffffecd9058d6cfa7d728
hmallen <allenhm@gmail.com>
2016-11-28 21:36:16 -0500

    Reorganized folders. Created shell scripts for webcam photo capture. Untested.

---
+++ up_camera.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PREFIX="UP-"
+SUFFIX=".jpg"
+DATE=$(date +"%Y-%m-%d_%H%M%S")
+
+FILENAME=$PREFIX$DATE$SUFFIX
+
+fswebcam --no-banner -q -i /dev/video1 -o ~/icarus_one/media/photos/$FILENAME.jpg
