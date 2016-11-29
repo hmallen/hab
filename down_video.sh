a45e6bcd3de305099f0f2cbdf7f4c4a66479d0c4
hmallen <allenhm@gmail.com>
2016-11-28 23:47:18 -0500

    Restructuring and more shell testing. Not functional.

--- down_video.sh
+++ down_video.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-VIDEOSOURCE=$0
-((LENGTHSEC=$1))
+VIDEOSOURCE=$1
+((LENGTHSEC=$2))
 ((FPS=12))
 ((LENGTH=FPS*LENGTHSEC))
+
+echo $VIDEOSOURCE
+echo $LENGTHSEC
+echo $FPS
+echo $LENGTH
 
 PREFIX="~/icarus_one/media/videos/DOWN-"
 DATE=$(date +"%m%d%Y-%H%M%S")
@@ -11,6 +16,6 @@ SUFFIX=".mp4"
 
 FILE=$PREFIX$DATE$SUFFIX
 
-sudo streamer -c /dev/video$VIDEOSOURCE -s 640x480 -f jpeg -t LENGTH -r FPS -j 75 -w 0 -o tmp/tmp.avi &&
+sudo streamer -c /dev/video$VIDEOSOURCE -s 640x480 -f jpeg -t $LENGTH -r FPS -j 75 -w 0 -o tmp/tmp.avi &&
 sudo avconv -i tmp/tmp.avi -preset ultrafast -crf 27 $FILE &&
 sudo rm tmp/tmp.avi
