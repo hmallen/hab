a265d82bff820312a5dc314ac793c11ae1ad8654
hmallen <allenhm@gmail.com>
2016-11-29 01:16:08 -0500

    Added lost file from testing copy.

---
+++ webcam_photo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" = "0" ]
+then
+CAMTYPE="UP-"
+else
+CAMTYPE="DOWN-"
+fi
+
+PREFIXDIR="media/photos/"
+PREFIX=$PREFIXDIR$CAMTYPE
+DATE=$(date +"%m%d%Y-%H%M%S")
+SUFFIX=".jpg"
+
+FILE=$PREFIX$DATE$SUFFIX
+
+sudo fswebcam --no-banner -r 1280x720 $FILE
