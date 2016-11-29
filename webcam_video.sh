b4feaf97124a1a6bfda5e0b1f8829c40ee137051
hmallen <allenhm@gmail.com>
2016-11-29 00:40:12 -0500

    Inclusion of webcam video shell script in program home directory.

---
+++ webcam_video.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VIDEOSOURCE=$1
+LENGTHSEC=$2
+FPS=15
+((LENGTH=FPS*LENGTHSEC))
+
+echo $VIDEOSOURCE
+echo $LENGTHSEC
+echo $FPS
+echo $LENGTH
+
+PREFIX="media/videos/DOWN-"
+DATE=$(date +"%m%d%Y-%H%M%S")
+SUFFIX=".mp4"
+
+FILE=$PREFIX$DATE$SUFFIX
+
+sudo streamer -c /dev/video$VIDEOSOURCE -s 640x480 -f jpeg -t $LENGTH -r $FPS -j 75 -w 0 -o tmp/tmp.avi &&
+sudo avconv -i tmp/tmp.avi -preset ultrafast -crf 27 $FILE &&
+sudo rm tmp/tmp.avi
