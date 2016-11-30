3d20e0ab86b1a8e8021b9023377c16c5a5fa41d0
hmallen <allenhm@gmail.com>
2016-11-29 19:24:02 -0500

    Removed avi conversions and storing raw webcam video files instead.

--- webcam_video.sh
+++ webcam_video.sh
@@ -13,10 +13,10 @@ fi
 
 PREFIX="media/videos/"
 DATE=$(date +"%m%d%Y-%H%M%S")
-SUFFIX=".mp4"
+SUFFIX=".avi"
 
 FILE=$PREFIX$CAMTYPE$DATE$SUFFIX
 
-sudo streamer -c /dev/video$1 -s 640x480 -f jpeg -t $LENGTH -r $FPS -j 75 -w 0 -o tmp/tmp.avi &&
-sudo nohup avconv -i tmp/tmp.avi -preset ultrafast -crf 27 $FILE &
-sudo rm tmp/tmp.avi
+sudo streamer -c /dev/video$1 -s 640x480 -f jpeg -t $LENGTH -r $FPS -j 75 -w 0 -o $FILE
+#sudo nohup avconv -i tmp/tmp.avi -preset ultrafast -crf 27 $FILE > webcam_video.out 2> webcam_video.err < /dev/null &
+#sudo rm tmp/tmp.avi
