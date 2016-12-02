bfa356fd82b754bb8f6e8b3bdc5d4ec829b6a948
hmallen <allenhm@gmail.com>
2016-12-02 17:38:05 -0500

    Added shell scripts for picamera capture.

---
+++ rpi_video.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+LENGTHSEC=$1
+((LENGTH=LENGTHSEC*1000))
+
+CAMTYPE="RPI-"
+
+PREFIX="media/videos/"
+DATE=$(date +"%m%d%Y-%H%M%S")
+SUFFIX=".h264"
+
+FILE=$PREFIX$CAMTYPE$DATE$SUFFIX
+
+raspivid -o $FILE -t $LENGTH
