8f92b0c63aa649cc8f69a77d9c078ee8e7edc973
hmallen <allenhm@gmail.com>
2016-12-15 14:14:01 -0500

    Finished new version of main camera control script. Untested.

--- Testing/python_testing/hab_camcontrol_NEW.py
+++ Testing/python_testing/hab_camcontrol_NEW.py
@@ -12,13 +12,27 @@ habSerial = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 camPi = 'rpi'
 camDown = 'down'
 camUp = 'up'
+
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
 camera.framerate = 15
 
-global videoStart
+global videoStartDown, videoStartUp
+videoStartDown = -120
+videoStartUp = -120
+#global camDownActive, camUpActive
+camDownActive = False
+camUpActive = False
 
-videoStart = -120
+captureInterval = 30
+
+######## MODE #########
+# 0 = Regular capture #
+# 1 = Takeoff         #
+# 2 = Peak            #
+# 3 = Landing         #
+#######################
+programMode = 0
 
 
 def capture_photo(camType):
@@ -58,6 +72,8 @@ def capture_photo(camType):
 
 
 def capture_video(camType, vidLength):
+    global videoStartDown, videoStartUp
+
     if camType == 'rpi':
         print 'PiCam video capture started.'
 
@@ -77,6 +93,7 @@ def capture_video(camType, vidLength):
         #error = std_err.strip('\n')
 
         print 'PiCam video capture finished.'
+
     elif camType == 'up':
         print 'Up-facing video capture started.'
         popenString = './webcam_video.sh 0 ' + str(vidLength)
@@ -84,6 +101,9 @@ def capture_video(camType, vidLength):
         std_out, std_err = popenCommand.communicate()
         status = std_out.strip('\n')
         error = std_err.strip('\n')
+
+        videoStartUp = timer()
+
     elif camType == 'down':
         print 'Down-facing video capture started.'
         popenString = './webcam_video.sh 1 ' + str(vidLength)
@@ -92,10 +112,12 @@ def capture_video(camType, vidLength):
         status = std_out.strip('\n')
         error = std_err.strip('\n')
 
+        videoStartDown = timer()
+
 
 while habSerial.inWaiting() > 0:
     habSerial.readline()
-    
+    sleep(0.005)
 
 programStart = False
 while programStart is False:
@@ -110,7 +132,94 @@ while programStart is False:
                     break
                 else:
                     print 'INVALID COMMAND RECEIVED.'
-                #elif habCommand == '-1':
-                    #print 'INVALID COMMAND RECEIVED.'
             else:
                 print habOutput
+
+while True:
+    loopStart = timer()
+
+    while (timer() - loopStart) < captureInterval:
+        # Regular serial reads to read incoming commands
+        if habSerial.inWaiting() > 0:
+            habOutput = habSerial.readline()[:-2]
+            if habOutput:
+                if habOutput[0] == '$':
+                    print 'Command received (Main): ' + habOutput
+                    if habOutput[1] == '0':
+                        programMode = 0
+                    elif habOutput[1] == '1':
+                        programMode = 1
+                    elif habOutput[1] == '2':
+                        programMode = 2
+                    elif habOutput[1] == '3':
+                        programMode = 3
+                else:
+                    print habOutput
+
+    # Mode-specific program functions
+    if programMode == 0:    # Regular capture
+        if (timer() - videoStartDown) > 120 and camDownActive == True:
+            camDownActive = False
+        if (timer() - videoStartUp) > 120 and camUpActive == True:
+            camUpActive = False
+
+        if camDownActive == False:
+            capture_photo(camDown)
+            sleep(1)
+        if camUpActive == False:
+            capture_photo(camUp)
+            sleep(1)
+
+    elif programMode == 1:  # Takeoff capture
+        if (timer() - videoStartDown) > 120 and camDownActive == True:
+            camDownActive = False
+        if (timer() - videoStartUp) > 120 and camUpActive == True:
+            camUpActive = False
+
+        if camDownActive == False and camUpActive == False:
+            capture_video(camDown, 120)
+            sleep(1)
+        if camUpActive == False:
+            capture_photo(camUp)
+            sleep(1)
+
+    elif programMode == 2:  # Peak capture
+        if (timer() - videoStartDown) > 120 and camDownActive == True:
+            camDownActive = False
+        if (timer() - videoStartUp) > 120 and camUpActive == True:
+            camUpActive = False
+
+        if camUpActive == False and camDownActive == False:
+            capture_video(camUp, 120)
+            sleep(1)
+        if camDownActive == False:
+            capture_photo(camDown)
+            sleep(1)
+
+    elif programMode == 3:  # Landing capture
+        if (timer() - videoStartUp) > 10 and camUpActive == False and camDownActive == False:
+            capture_video(camUp, 10)
+            sleep(1)
+            camUpActive = True
+        elif (timer() - videoStartUp) > 10 and camUpActive == True:
+            camUpActive = False
+
+        if (timer() - videoStartDown) > 120 and camDownActive == False and camUpActive == False:
+            capture_video(camDown, 120)
+            sleep(1)
+            camDownActive = True
+        elif (timer() - videoStartDown) > 120 and camDownActive == True:
+            camDownActive = False
+
+        if camDownActive == False:
+            capture_photo(camDown)
+            sleep(1)
+        elif camUpActive == False:
+            capture_photo(camUp)
+            sleep(1)
+
+    capture_photo(camPi)
+    sleep(1)
+
+    #sleep(captureInterval) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Also implement timeout breaks from current phase
